ef65fdd54df4175f1c468c3a01386f236861b8ee
jairomachado07 <jr.hk22@gmail.com>
2023-10-31 19:59:58 -0300

    Initial commit

---
+++ app/src/main/java/com/academia/MainActivity.java
@@ -0,0 +1,17 @@
+package com.academia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText academia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

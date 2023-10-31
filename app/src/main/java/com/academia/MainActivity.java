3c46160509126dafca9e91a98f6142d34ec1774b
jairomachado07 <jr.hk22@gmail.com>
2023-10-31 20:13:03 -0300

    Inserção de uma nova tela (principal).

--- app/src/main/java/com/academia/MainActivity.java
+++ app/src/main/java/com/academia/MainActivity.java
@@ -2,16 +2,31 @@ package com.academia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText academia;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Principal.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
